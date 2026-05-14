1556d66b979c21a877c21db907ab6607607b6ed5
Alisher <alisherr404@gmail.com>
2026-05-14 17:08:56 +0500

    fix cors syntax

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const db       = require('./db');
 const app  = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(cors({ origin: '*', methods: ['GET','POST','PUT','DELETE','OPTIONS'], allowedHeaders: ['Content-Type','Authorization','
+app.use(cors());
 app.use(express.json());
 
 // ============================================
